5419b33ffe007debce688b60247ee76f2231a03d
SD_IT <72951311+AleksSad@users.noreply.github.com>
2024-03-11 11:47:06 +0500

    /

--- dash/scripts/main_script.js
+++ dash/scripts/main_script.js
@@ -109,7 +109,9 @@ function view_stats(data) {
     krus_n_m_stats.innerHTML = count_krus_n_m;
     krus_n_l_stats.innerHTML = count_krus_n_l;
 
-    work_stats.innerHTML = bss_n_stats + krus_n_stats + krus_y_stats;
+    var work_stats_ = bss_n_stats + krus_n_stats + krus_y_stats;
+    work_stats.innerHTML = work_stats_;
+    work_stats_ = null;
     // view_table(data);
 }
 function view_filter(filter_th) {
